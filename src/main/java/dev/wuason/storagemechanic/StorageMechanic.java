9083ac1457f468c425033bf65d706e600e8baac3
Wuason6x9 <daniperezmartinez48@gmail.com>
2023-04-16 20:22:56 +0200

    0.1.3

--- src/main/java/dev/wuason/storagemechanic/StorageMechanic.java
+++ src/main/java/dev/wuason/storagemechanic/StorageMechanic.java
@@ -1,12 +1,24 @@
 package dev.wuason.storagemechanic;
 
+import dev.wuason.mechanics.Mechanics;
+import dev.wuason.mechanics.utils.MechanicsUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StorageMechanic extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+
+        if(!MechanicsUtils.isMechanicLoaded(this)){
+
+            getLogger().severe("ERROR LOADING STORAGE MECHANIC! ");
+            getLogger().severe("You must not put the plugin in the following folder!");
+            getLogger().severe(Mechanics.getInstance().getDataFolder().getPath() + "/mechanics");
+
+            getPluginLoader().disablePlugin(this);
+
+
+        }
 
     }
 
