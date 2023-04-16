afa84055237e0daad801594e5634420e95129880
Wuason6x9 <daniperezmartinez48@gmail.com>
2023-04-16 13:36:37 +0200

    0.1

---
+++ src/main/java/dev/wuason/storagemechanic/StorageMechanic.java
@@ -0,0 +1,17 @@
+package dev.wuason.storagemechanic;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StorageMechanic extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
